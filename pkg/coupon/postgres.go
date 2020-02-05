SphericalKat/hades

commit dc683285d83d8a45e14070ee009a0acbe6f5dd1b

coupon: Rework logic for redemption
Signed-off-by: ATechnoHazard <amolele@gmail.com>

pkg/coupon/postgres.go
@@ -18,7 +18,7 @@ func (r *repo) SaveCoupon(coupon *entities.Coupon) error {
 	tx := r.DB.Begin()
 	eve := &entities.Event{ID: coupon.EventId}
 
-	switch tx.Find(eve).Error {
+	switch tx.Where("id = ?", coupon.EventId).Find(eve).Error {
 	case gorm.ErrRecordNotFound:
 		tx.Rollback()
 		return pkg.ErrNotFound
@@ -46,61 +46,86 @@ func (r *repo) DeleteCoupon(couponId uint) error {
 	}
 }
 
-func (r *repo) RemoveCouponParticipant(couponId uint, regNo string) error {
+func (r *repo) RedeemCoupon(couponId uint, regNo string) error {
 	tx := r.DB.Begin()
 	p := &entities.Participant{RegNo: regNo}
 	c := &entities.Coupon{}
+	var parts []entities.Participant
 
+	// Find the coupon in db
 	if err := tx.Where("coupon_id = ?", couponId).Find(c).Error; err != nil {
+		tx.Rollback()
 		switch err {
 		case gorm.ErrRecordNotFound:
-			tx.Rollback()
 			return pkg.ErrNotFound
 		default:
-			tx.Rollback()
 			return pkg.ErrDatabase
 		}
 	}
 
-	switch tx.Where("reg_no = ?", p.RegNo).Find(p).Error {
-	case gorm.ErrRecordNotFound:
+	// Find participant in db
+	if err := tx.Where("reg_no = ?", regNo).Find(p).Error; err != nil {
 		tx.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			return pkg.ErrNotFound
+		default:
+			return pkg.ErrDatabase
+		}
+	}
+
+	// Check if already redeemed
+	if err := tx.Model(c).Association("Participants").Find(&parts).Error; err != nil {
+		tx.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			return pkg.ErrNotFound
+		default:
+			return pkg.ErrDatabase
+		}
+	}
+
+	if parts == nil {
 		return pkg.ErrNotFound
+	}
+	for _, part := range parts {
+		if part.RegNo == regNo {
+			return pkg.ErrAlreadyExists
+		}
+	}
 
-	case nil:
-		var coups []entities.Coupon
-		err := tx.Model(p).Association("Coupons").Find(&coups).Error
-
+	// verify that the participant is part of the event
+	if err := tx.Model(p).Association("Events").Find(&p.Events).Error; err != nil {
+		tx.Rollback()
 		switch err {
 		case gorm.ErrRecordNotFound:
-			tx.Rollback()
 			return pkg.ErrNotFound
-		case nil:
-			flag := false
-			for _, cp := range coups {
-				if cp.CouponId == couponId {
-					flag = true
-				}
-			}
-			if !flag {
-				return pkg.ErrNotFound
-			}
-			err := tx.Find(p).Association("Coupons").Delete(c).Error
-			if err != nil {
-				return pkg.ErrDatabase
-			}
-
-			tx.Commit()
-			return nil
 		default:
-			tx.Rollback()
 			return pkg.ErrDatabase
 		}
-
-	default:
-		tx.Rollback()
-		return pkg.ErrDatabase
 	}
+	flag := false
+	for _, event := range p.Events {
+		if event.ID == couponId {
+			flag = true
+		}
+	}
+
+	if !flag {
+		return pkg.ErrInvalidSlug
+	}
+
+	// Redeem coupon by adding to association
+	if err := tx.Model(c).Association("Participants").Append(&p).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			return pkg.ErrNotFound
+		default:
+			return pkg.ErrDatabase
+		}
+	}
+
+	return nil
 }
 
 func (r *repo) GetCoupons(eventId uint, day uint) ([]entities.Coupon, error) {
@@ -115,58 +140,6 @@ func (r *repo) GetCoupons(eventId uint, day uint) ([]entities.Coupon, error) {
 	default:
 		return nil, pkg.ErrDatabase
 	}
-}
-
-func (r *repo) AddCouponsToAll(eventId uint) error {
-	eve := &entities.Event{ID: eventId}
-	var peeps []entities.Participant
-	var coups []entities.Coupon
-
-	tx := r.DB.Begin()
-	err := tx.Where("id = ?", eventId).Find(eve).Error
-
-	if err == gorm.ErrRecordNotFound {
-		tx.Rollback()
-		return pkg.ErrNotFound
-	} else if err != nil {
-		tx.Rollback()
-		return pkg.ErrDatabase
-	}
-
-	// populate coupons by event id.
-	err = tx.Model(entities.Coupon{}).Where("event_id = ?", eventId).Find(&coups).Error
-	if err == gorm.ErrRecordNotFound {
-		tx.Rollback()
-		return pkg.ErrNotFound
-	} else if err != nil {
-		tx.Rollback()
-		return pkg.ErrDatabase
-	}
-
-	// populate participants in the event.
-	err = tx.Model(eve).Association("Attendees").Find(&peeps).Error
-	if err == gorm.ErrRecordNotFound {
-		tx.Rollback()
-		return pkg.ErrNotFound
-	} else if err != nil {
-		tx.Rollback()
-		return pkg.ErrDatabase
-	}
-
-	for _, coup := range coups {
-		for _, peep := range peeps {
-			err = tx.Model(peep).Association("Coupons").Append(coup).Error
-			if err == gorm.ErrRecordNotFound {
-				tx.Rollback()
-				return pkg.ErrNotFound
-			} else if err != nil {
-				tx.Rollback()
-				return pkg.ErrDatabase
-			}
-		}
-	}
-	tx.Commit()
-	return nil
 }
 
 func (r *repo) VerifyCoupon(eventId uint, couponId uint) (bool, error) {

api/handler/coupon.go
@@ -139,7 +139,7 @@ func redeemCoupon(couponService coupon.Service, eventService event.Service) http
 			return
 		}
 
-		if err := couponService.RemoveCouponParticipant(composite.CouponID, composite.RegNo); err != nil {
+		if err := couponService.RedeemCoupon(composite.CouponID, composite.RegNo); err != nil {
 			views.Wrap(err, w)
 			return
 		}
@@ -148,39 +148,7 @@ func redeemCoupon(couponService coupon.Service, eventService event.Service) http
 	}
 }
 
-func addAllCoupons(couponService coupon.Service, eventService event.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		coup := &entities.Coupon{}
-
-		ctx := r.Context()
-		tk := ctx.Value(middleware.JwtContextKey("token")).(*middleware.Token)
-
-		if err := json.NewDecoder(r.Body).Decode(coup); err != nil {
-			views.Wrap(err, w)
-			return
-		}
-
-		eve, err := eventService.ReadEvent(coup.EventId)
-		if err != nil {
-			views.Wrap(err, w)
-			return
-		}
-
-		if tk.OrgID != eve.OrganizationID {
-			u.Respond(w, u.Message(http.StatusForbidden, "You are forbidden from modifying this resource."))
-			return
-		}
-
-		if err := couponService.AddCouponsToAll(eve.ID); err != nil {
-			views.Wrap(err, w)
-			return
-		}
-
-		u.Respond(w, u.Message(http.StatusOK, "Added coupons to all participants successfully."))
-	}
-}
-
-func GetCoupons(couponService coupon.Service, eventService event.Service) http.HandlerFunc {
+func getCoupons(couponService coupon.Service, eventService event.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		coup := &entities.Coupon{}
 		ctx := r.Context()
@@ -226,8 +194,6 @@ func MakeCouponHandler(r *httprouter.Router, couponService coupon.Service, event
 		middleware.JwtAuthentication(j.GetHandler(deleteCoupon(couponService, eventService))))
 	r.HandlerFunc("POST", "/api/v2/coupon/redeem-coupon",
 		middleware.JwtAuthentication(redeemCoupon(couponService, eventService)))
-	r.HandlerFunc("POST", "/api/v2/coupon/add-all-coupons",
-		middleware.JwtAuthentication(j.GetHandler(addAllCoupons(couponService, eventService))))
 	r.HandlerFunc("POST", "/api/v2/coupon/get-coupons",
-		middleware.JwtAuthentication(GetCoupons(couponService, eventService)))
+		middleware.JwtAuthentication(getCoupons(couponService, eventService)))
 }

pkg/coupon/repository.go
@@ -4,9 +4,8 @@ import "github.com/ATechnoHazard/hades-2/pkg/entities"
 
 type Repository interface {
 	SaveCoupon(*entities.Coupon) error
-	RemoveCouponParticipant(uint, string) error
+	RedeemCoupon(uint, string) error
 	DeleteCoupon(uint) error
 	GetCoupons(uint, uint) ([]entities.Coupon, error)
-	AddCouponsToAll(uint) error
 	VerifyCoupon(uint, uint) (bool, error)
 }

pkg/coupon/service.go
@@ -4,10 +4,9 @@ import "github.com/ATechnoHazard/hades-2/pkg/entities"
 
 type Service interface {
 	SaveCoupon(*entities.Coupon) error
-	RemoveCouponParticipant(uint, string) error
+	RedeemCoupon(uint, string) error
 	DeleteCoupon(uint) error
 	GetCoupons(uint, uint) ([]entities.Coupon, error)
-	AddCouponsToAll(uint) error
 	VerifyCoupon(uint, uint) (bool, error)
 }
 
@@ -23,8 +22,8 @@ func (s *couponSvc) SaveCoupon(coupons *entities.Coupon) error {
 	return s.repo.SaveCoupon(coupons)
 }
 
-func (s *couponSvc) RemoveCouponParticipant(couponId uint, regNo string) error {
-	return s.repo.RemoveCouponParticipant(couponId, regNo)
+func (s *couponSvc) RedeemCoupon(couponId uint, regNo string) error {
+	return s.repo.RedeemCoupon(couponId, regNo)
 }
 
 func (s *couponSvc) DeleteCoupon(couponId uint) error {
@@ -33,10 +32,6 @@ func (s *couponSvc) DeleteCoupon(couponId uint) error {
 
 func (s *couponSvc) GetCoupons(eventId uint, day uint) ([]entities.Coupon, error) {
 	return s.repo.GetCoupons(eventId, day)
-}
-
-func (s *couponSvc) AddCouponsToAll(eventId uint) error {
-	return s.repo.AddCouponsToAll(eventId)
 }
 
 func (s *couponSvc) VerifyCoupon(eventId uint, couponId uint) (bool, error) {

commit 22ff312dcd0832ec0a178fda2bfeb9ff8b0a55cc

coupons: Fix a bug introduced in dc68328
Fixes #2

Signed-off-by: ATechnoHazard <amolele@gmail.com>

pkg/coupon/postgres.go
@@ -109,7 +109,7 @@ func (r *repo) RedeemCoupon(couponId uint, regNo string) error {
 	}
 	flag := false
 	for _, event := range p.Events {
-		if event.ID == couponId {
+		if event.ID == c.EventId {
 			flag = true
 		}
 	}